c10b606a12232efbb081a37150a559ca191c427a
Sokomine <wegwerf@anarres.dyndns.org>
2014-07-20 23:12:08 +0200

    added support for rotated nodes

--- init.lua
+++ init.lua
@@ -2,8 +2,6 @@
 moresnow = {}
 
 
--- TODO: only place special nodes on top if facedir < 4 (at least for roof nodes); else treat them like nodeboxes
--- TODO: some shapes of rotated stairs and slabs are aequivalent to a solid node; check that?
 moresnow.on_construct = function( pos )
 	
 	local posb = {x=pos.x, y=pos.y-1, z=pos.z};
@@ -22,9 +20,63 @@ moresnow.on_construct = function( pos )
 
 		suggested = minetest.get_name_from_content_id( suggested );
 
+		-- no rule for this node
+		if( not( suggested )) then
+			return;
+		end
+
+		local p2 = node.param2;
+		-- if this is a stair or a roof node from homedecor or technics cnc machine;
+		-- those nodes are all comparable regarding rotation
+		if(     suggested == 'moresnow:snow_stair_top' or suggested == 'moresnow:snow_ramp_top' ) then
+			if(     p2==5 or p2==7 or p2==9  or p2==11 or p2==12 or p2==14 or p2==16 or p2==18 ) then
+				suggested = 'moresnow:snow_top';
+			-- stair turned upside down
+			elseif( p2==6 or p2==8 or p2==15 or p2==17 or p2==20 or p2==21 or p2==22 or p2==23) then
+				suggested = 'default:snow';
+			-- all these transform into stairs; however, adding the offset (our snow node lies lower than a normal node) would cause chaos
+			elseif( p2 ==19) then
+				p2 = 1;
+			elseif( p2 ==4 ) then
+			        p2 = 2;
+			elseif( p2 ==13) then
+			        p2 = 3;
+			elseif( p2 ==10) then
+			        p2 = 0;
+			-- else it really is a stiar
+			end
+		elseif( suggested == 'moresnow:snow_slab_top' ) then
+			-- vertical slab; tread as a nodebox
+			if(     p2 >= 4  and p2 <= 19 ) then
+				suggested = 'moresnow:snow_top';
+			-- slab turned upside down
+			elseif( p2 >= 20 and p2 <= 23 ) then
+				suggested = 'default:snow';
+			-- else it's a slab
+			end
+		
+		elseif( suggested == 'moresnow:snow_ramp_outer_top' ) then
+			-- treat like a nodebox
+			if(     p2>=4    and p2 <= 19 ) then
+				suggested = 'moresnow:snow_top';
+			-- upside-down
+			elseif( p2 >= 20 and p2 <= 23 ) then
+				suggested = 'default:snow';
+			end
+		
+		elseif( suggested == 'moresnow:snow_ramp_inner_top' ) then
+			-- treat like a nodebox
+			if(     p2>=4    and p2 <= 19 ) then
+				suggested = 'moresnow:snow_top';
+			-- upside-down
+			elseif( p2 >= 20 and p2 <= 23 ) then
+				suggested = 'default:snow';
+			end
+		end
+
 		-- snow_top is a special node suitable for nodeboxes; BUT: it only looks acceptable if the
 		-- node below that nodebox/torch/fence/etc is a solid one
-		if( suggested and suggested == 'moresnow:snow_top' ) then
+		if( suggested == 'moresnow:snow_top' ) then
 			local node2      = minetest.get_node( {x=pos.x, y=pos.y-2, z=pos.z});
 			-- no information about the node below available - we don't know what to do
 			if( not( node2 ) or node2.name == 'air' or node2.name == 'ignore' ) then
@@ -55,8 +107,19 @@ moresnow.on_construct = function( pos )
 
 		if( suggested ) then
 			local old = minetest.get_node( pos );
-			if( old and old.name and (old.name ~= suggested or ( node.param2 and old.param2 ~= node.param2))) then
-				minetest.set_node( pos, { name = suggested, param2 = node.param2} );
+
+			if( suggested == 'default:snow' ) then
+				-- if there is snow already, make it higher
+				if( old and old.name and old.name == suggested ) then
+					p2 = old.param2 + 10;
+				-- prevent the snow from getting higher
+				else
+					p2 = 0;
+				end
+			end
+			if( old and old.name and (old.name ~= suggested or ( old.param2 and old.param2 ~= p2))) then
+-- swap_node does not seem to affect param2
+				minetest.swap_node( pos, { name = suggested, param2 = p2} );
 			end
 		end
 	end
